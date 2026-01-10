500542eae9d11e4dd610e34f282dc8e3dbf12e38
Fatima Naz <naz781@gmail.com>
2026-01-10 14:58:22 -0500

    added animation on hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Footer from './components/Footer.jsx'
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-    <div className='flex-center h-screen'>
-      <h1 className="text-3xl text-indigo-300"> hello World</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'></div>
+      <Footer />
+    </main>
   )
 }
 
